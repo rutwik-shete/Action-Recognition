9bf1e2b237f4b41e898f84cd17f59bbe15b425b5
Minz <minzjdev@gmail.com>
2023-05-24 11:07:27 +0100

    latest update

--- model.py
+++ model.py
@@ -13,18 +13,18 @@ from models.Resnet18_3D import Resnet18_3D_With_Attention
 from models.Timesformer600 import timeSformer600
 from models.Resnet18_2D import Resnet18_2D_With_Attention
 from models.VideoMAE import VideoMAE
+import torch.nn.init as init
+
 
 def timeSformer400():
-
     processor = AutoImageProcessor.from_pretrained("facebook/timesformer-base-finetuned-k400")
     model = TimesformerForVideoClassification.from_pretrained("facebook/timesformer-base-finetuned-k400")
 
     for params in model.parameters():
-        params.requires_grad = False
+        params.requires_grad = True
 
     model.classifier = nn.Linear(768,len(CATEGORY_INDEX),bias=True)
 
-    return model,processor
 
 def getModel(args):
     print("Proceeding with Model:", args.model)
